dd7b1014507f0bfd959547ba3e3ec1e2a356a6e3
chiabi <chiabi88@gmail.com>
2018-05-10 01:24:43 +0900

    세로, 대각선 일치하는 지 확인하는 부분 추가, 무승부 경우 추가, restart 버튼, 이겼을 때 이벤트 추가

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ class TicTacToe {
   player = 'X';
   // 동작?
   // - 턴
+  init() {
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    this.player = 'X';
+  }
   turn({row, col}) {
     if (this.board[row][col] == null) {
       // 현재 플레이어에대한 표시를 게임판의 해당 위치에 넣어주고
@@ -21,24 +29,49 @@ class TicTacToe {
   }
   // - 누가 이겼는지 판별
   checkWinner() {
-    // 가로로 모두
+    let noNull = 0;
     for (let i = 0; i < 3; i++) {
       if (
+        // 가로로 모두
         this.board[i][0] != null && 
         this.board[i][0] === this.board[i][1] && 
         this.board[i][1] === this.board[i][2]
       ) {
         return this.board[i][0];
+      } else if (
+        // 세로로 모두
+        this.board[0][i] != null &&
+        this.board[0][i] === this.board[1][i] && 
+        this.board[1][i] === this.board[2][i]
+      ) {
+        return this.board[0][i];
+      } 
+      for(let j = 0; j < 3; j++) {
+        if (this.board[i][j] != null) {
+          noNull++;
+        }
       }
     }
-    // 세로로 모두
-    // 대각선 모두
+    if (
+      // 대각선 모두
+      this.board[1][1] != null &&
+      (
+        this.board[0][0] === this.board[1][1] &&
+        this.board[0][0] === this.board[2][2]
+      ) || (
+        this.board[0][2] === this.board[1][1] &&
+        this.board[0][2] === this.board[2][0]
+    )) {
+      return this.board[1][1];
+    } else if(noNull === 9) {
+      return 'Draw';
+    }
   }
 }
 const game = new TicTacToe();
 
 const rowEls = document.querySelectorAll('.board__row'); 
-
+const winnerEls = document.querySelector('.winner');
 rowEls.forEach((rowEl, rowIndex) => {
   const colEls = rowEl.querySelectorAll('.board__col');
   colEls.forEach((colEl, colIndex) => {
@@ -48,22 +81,16 @@ rowEls.forEach((rowEl, rowIndex) => {
       e.target.textContent = game.board[rowIndex][colIndex];
       const winner = game.checkWinner();
       if(winner) {
-        document.querySelector('.winner').textContent = winner;
+        winnerEls.classList.add('end');
+        winnerEls.querySelector('.winner__txt').textContent = winner;
       }
     });
   });
 });
-
-function draw() {
-  game.board.forEach(((rowArr, rowIndex) => {
-    const rowEl = rowEls[rowIndex];
-    const colEls = rowEl.querySelectorAll('.board__col');
-    rowArr.forEach((col, colIndex) => {
-      colEls[colIndex].textContent = col;
-    });
-  }));
-  const winner = game.checkWinner();
-  if(winner) {
-    document.querySelector('.winner').textContent = winner;
-  }
-}
+document.querySelector('.btn-restart').addEventListener('click', e => {
+  game.init();
+  document.querySelectorAll('.board__col').forEach(colEl => {
+    colEl.textContent = '';
+  });
+  winnerEls.classList.remove('end');
+});
